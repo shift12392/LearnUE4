6becfa4fb2ee466a3348bd8cf980f319cc8d9ab0
lu <shift12392@163.com>
2020-04-12 12:28:24 +0800

    添加

---
+++ LearnUE4/Source/LearnUE4/LearnUE4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearnUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUE4_API ALearnUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
